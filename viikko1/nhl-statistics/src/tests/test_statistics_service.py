fddac49907fedd6fe8e4d0cb8f6db76316345db3
Karaatsar <kara.atsar@helsinki.fi>
2025-11-06 10:42:13 +0200

    testien muutoksia

--- viikko1/nhl-statistics/src/tests/test_statistics_service.py
+++ viikko1/nhl-statistics/src/tests/test_statistics_service.py
@@ -2,15 +2,20 @@ from statistics_service import StatisticsService
 
 from player import Player
 
-def test_two_consecutive_calculations():
-    players=[
-        Player("Saku", "MTL", 10, 20),
-        Player("Teemu", "ANA", 15, 25)
-    ]
-    stats= StatisticsService(players)
+class PlayerReaderStub:
+    def get_players(self):
+        return [
+            Player("Saku", "MTL", 10, 20),
+            Player("Teemu", "ANA", 15,25)
+        ]
+    
+    def test_two_calculations():
+        reader_stub= PlayerReaderStub()
+        stats = StatisticsService()
+        stats._players = reader_stub.get_players()
 
-    player1=stats.search("Saku")
-    player2=stats.search("Teemu")
+        player1=stats.search("Saku")
+        player2=stats.search("Teemu")
 
-    assert player1.points(30)
-    assert player2.points(40)
+        assert player1.points==30
+        assert player2.points==40
